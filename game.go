5e6527c47c7ec9458270974dba5ec6be564b8f4f
Stuart McTaggart <stuartmctaggart@gmail.com>
2017-04-22 11:28:50 +0100

    new

--- game.go
+++ game.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 )
 
@@ -31,14 +32,14 @@ func NewGame(width, height int) *Game {
 func (game *Game) Setup() {
 
 	// Configure the vertex and fragment shaders
-	prog, err := NewShaderProgram("./shaders/vertex_basic.vert", "./shaders/frag_basic.frag")
+	prog, err := NewShaderProgram("./shaders/basic_tex.vert", "./shaders/basic_tex.frag")
 	if err != nil {
 		panic(err)
 	}
 	game.Program = prog
 
 	// Load the textures
-	tex, err := LoadTextures("./textures")
+	tex, err := LoadTextures("./texture")
 	if err != nil {
 		panic(err)
 	}
@@ -53,18 +54,58 @@ func (game *Game) Setup() {
 	var VBO uint32
 	gl.GenBuffers(1, &VBO)
 	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*floatSize, gl.Ptr(vertices), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, len(verticesCube)*floatSize, gl.Ptr(verticesCube), gl.STATIC_DRAW)
+
+	/*var EBO uint32
+	gl.GenBuffers(1, &EBO)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indicesRect)*floatSize, gl.Ptr(indicesRect), gl.STATIC_DRAW)*/
 
 	// Coords Attributes
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, int32(3*floatSize), gl.PtrOffset(0))
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, int32(5*floatSize), gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(0)
+	// Color attribute
+	/*gl.VertexAttribPointer(1, 3, gl.FLOAT, false, int32(8*floatSize), gl.PtrOffset(3*floatSize))
+	gl.EnableVertexAttribArray(1)*/
+	// TexCoord attribute
+	gl.VertexAttribPointer(2, 2, gl.FLOAT, false, int32(5*floatSize), gl.PtrOffset(3*floatSize))
+	gl.EnableVertexAttribArray(2)
+
+	gl.BindVertexArray(0)
 
 }
 
 func (game *Game) Render() {
 	gl.UseProgram(game.Program)
+
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, game.Textures["container.jpg"])
+	gl.Uniform1i(gl.GetUniformLocation(game.Program, gl.Str("texture1\x00")), 0)
+
+	gl.ActiveTexture(gl.TEXTURE1)
+	gl.BindTexture(gl.TEXTURE_2D, game.Textures["awesomeface.png"])
+	gl.Uniform1i(gl.GetUniformLocation(game.Program, gl.Str("texture2\x00")), 1)
+
 	gl.BindVertexArray(game.VAO)
-	gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+	for _, pos := range positions {
+		model0 := mgl32.Translate3D(pos.X(), pos.Y(), pos.Z())
+		model1 := mgl32.HomogRotate3DX(mgl32.DegToRad(float32(glfw.GetTime() * 50.0)))
+		model2 := mgl32.HomogRotate3DY(mgl32.DegToRad(float32(glfw.GetTime() * 50.0)))
+		model := model0.Mul4(model1).Mul4(model2)
+		view := mgl32.Translate3D(0, 0, -4.0)
+
+		projection := mgl32.Perspective(mgl32.DegToRad(45.0), 800/600, 0.1, 100.0)
+
+		gl.UniformMatrix4fv(gl.GetUniformLocation(game.Program, gl.Str("model\x00")), 1, false, &model[0])
+		gl.UniformMatrix4fv(gl.GetUniformLocation(game.Program, gl.Str("view\x00")), 1, false, &view[0])
+		gl.UniformMatrix4fv(gl.GetUniformLocation(game.Program, gl.Str("projection\x00")), 1, false, &projection[0])
+
+		gl.DrawArrays(gl.TRIANGLES, 0, 36)
+	}
+
+	//gl.BindTexture(gl.TEXTURE_2D, game.Textures["container.jpg"])
+	//gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.Ptr(nil))
 	gl.BindVertexArray(0)
 }
 
@@ -142,4 +183,17 @@ var verticesCube = []float32{
 	0.5, 0.5, 0.5, 1.0, 0.0,
 	-0.5, 0.5, 0.5, 0.0, 0.0,
 	-0.5, 0.5, -0.5, 0.0, 1.0,
+}
+
+var positions = []mgl32.Vec3{
+	mgl32.Vec3{0.0, 0.0, 0.0},
+	mgl32.Vec3{2.0, 5.0, -15.0},
+	mgl32.Vec3{-1.5, -2.2, -2.5},
+	mgl32.Vec3{-3.8, -2.0, -12.3},
+	mgl32.Vec3{2.4, -0.4, -3.5},
+	mgl32.Vec3{-1.7, 3.0, -7.5},
+	mgl32.Vec3{1.3, -2.0, -2.5},
+	mgl32.Vec3{1.5, 2.0, -2.5},
+	mgl32.Vec3{1.5, 0.2, -1.5},
+	mgl32.Vec3{-1.3, 1.0, -1.5},
 }
